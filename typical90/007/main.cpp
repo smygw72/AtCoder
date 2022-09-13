60370cd3270e802eaa15fa922c9930588970c574
smygw72 <smygw72@gmail.com>
2022-09-14 01:27:25 +0900

    solved

--- typical90/007/main.cpp
+++ typical90/007/main.cpp
@@ -19,22 +19,83 @@ using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
 using vi = vector<int>;
+using si = stack<int>;
+using qi = queue<int>;
+using li = list<int>;
 
-const int mod = 1e9 + 7;
-const long double pi = 3.141592653589793238462643383279502884197;
-const long double eps = 1e-7;
+static const int INF = (1 << 21);
+static const long double PI = 3.141592653589793238462643383279502884197;
+static const long double EPS = 1e-7;
+static const int MAX = 300000;
 
-void print_vector(vi vec)
+struct Student
 {
-    REP(i, vec.size())
+    int index;
+    int B;
+
+    // 最後のconstを忘れると"instantiated from here"というエラーが出てコンパイルできないので注意
+    bool operator<(const Student &right) const
     {
-        cout << vec[i] << " ";
+        return B < right.B;
+    }
+};
+
+ll A[MAX];
+Student student[MAX];
+int N, Q;
+
+int search(const ll student_rate, ll &student_score, int &left)
+{
+    REPS(i, left, N)
+    {
+        ll left_score = abs(student_rate - A[i]);
+        if (i == N - 1)
+        {
+            student_score = left_score;
+            return i;
+        }
+        ll right_score = abs(student_rate - A[i + 1]);
+        if (left_score < right_score)
+        {
+            student_score = left_score;
+            return i;
+        }
     }
 }
 
 int main()
 {
-    string s;
-    cin >> s;
+    cin >> N;
+    REP(i, N)
+    {
+        cin >> A[i];
+    }
+    cin >> Q;
+
+    sort(A, A + N);
+
+    REP(i, Q)
+    {
+        student[i].index = i;
+        cin >> student[i].B;
+    }
+
+    sort(student, student + Q);
+
+    int left = 0;
+
+    ll scores[MAX];
+    REP(i, Q)
+    {
+        ll score;
+        left = search(student[i].B, score, left);
+        scores[student[i].index] = score;
+    }
+
+    REP(i, Q)
+    {
+        cout << scores[i] << endl;
+    }
+
     return 0;
 }
